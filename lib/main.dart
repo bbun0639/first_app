5598b7046e6c162d4806d2c38c2a6c6532900b6a
bbun0639 <pathomphob_sun@cmu.ac.th>
2019-12-29 14:20:09 +0700

    Update main.dart
    Building a widget Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget { //inheritance by 'extends'
+class MyApp extends StatelessWidget {
+  //inheritance by 'extends'
   @override //decorator
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello! This is my first App'),);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }
