807778e169c59804ce979f65d81cc56c333e05fd
bbun0639 <pathomphob_sun@cmu.ac.th>
2020-02-02 23:56:10 +0700

    "if" Statement and not error when empty answer

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-
-  final Function selectHandler;//Add 'final property' that store
+  final Function
+      selectHandler; //Add 'final property' that store //final is a vlaue doesn't change
   final String answerText;
 
-  Answer(this.selectHandler, this.answerText); //Add constructor (shortcut) by repeating class name
+  Answer(this.selectHandler,
+      this.answerText); //Add constructor (shortcut) by repeating class name
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        child: Text(answerText),
+        child: Text(
+          answerText,
+          style: TextStyle(fontSize: 20),
+        ),
         color: Colors.blue,
         textColor: Colors.white,
         onPressed: selectHandler,
@@ -20,3 +24,5 @@ class Answer extends StatelessWidget {
     );
   }
 }
+
+class TextSize {}
--- lib/main.dart
+++ lib/main.dart
@@ -27,21 +27,7 @@ class _MyAppState extends State<MyApp> {
 
   var _questionIndex = 0;
 
-  void _answerQuestions() {
-    //setState is function to provided by the State<MyApp> inherit(ถ่ายทอด)
-    //setState is function that forces Flutter to re-render the UI
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-
-    print(_questionIndex);
-    //print('Any Answer chosen!');
-  }
-
-  //inheritance by 'extends'
-  @override //decorator
-  Widget build(BuildContext context) {
-    var questions = [
+  final questions = const [                         //Add 'const' in front of variable for value is const in Dart
       {
         'questionText': 'What\'s your fav color?',    //Map = key-value 'pair'
         'answer': ['Black', 'White', 'Green', 'Red',],
@@ -55,12 +41,42 @@ class _MyAppState extends State<MyApp> {
         'answer': ['Max', 'Jack', 'Rack',],
       },
     ];
+
+  void _answerQuestions() {
+    //setState is function to provided by the State<MyApp> inherit(ถ่ายทอด)
+    //setState is function that forces Flutter to re-render the UI
+    
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    if (_questionIndex < questions.length) {
+      print('We have more question!');
+    } else {
+      print('No more question!');
+    }
+
+    print(_questionIndex);
+    //print('Any Answer chosen!');
+  }
+
+  //inheritance by 'extends'
+  @override //decorator
+  Widget build(BuildContext context) {
+    
+    //questions = [];   //can't be assigned new object if questions is a 'const' (it could var .... const []) 
+
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];   //if you have a list and reuse the variable then assign a brand new vakue to allowed use the variable
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
+        body: _questionIndex < questions.length ? Column(  // Use '?' for mark border to the code which if this TRUE
           children: [
             Question(
               //It's constructor to get value for the questionText
@@ -72,16 +88,22 @@ class _MyAppState extends State<MyApp> {
               return Answer(_answerQuestions, answer);                                //'List<String>' to tell Dart 'answer' is a list
             }).toList() //Convert value in map() to list    //... take a list that we habe & pull all value in the list
 
+
+
             // Answer(_answerQuestions), //ต้องไปเพิ่ม  Constructor ด้วยเพื่อยอมรับการ Callback
             // Answer(_answerQuestions), //Answer จะไป Pointer at a Function Answer
             // RaisedButton(
             //   child: Text('Answer 2'),
-            //   color: Colors.deepOrange,
+            //   color: Colors.deepOrange
             //   onPressed: () =>
             //       print('Answer 2 chosen!'), //Anonymous () function
             // ),
           ],
-        ),
+        ) : Center(
+            child: Text(
+              'No more question!', 
+              style: TextStyle(fontSize: 28),) 
+          ),
       ),
     );
   }
